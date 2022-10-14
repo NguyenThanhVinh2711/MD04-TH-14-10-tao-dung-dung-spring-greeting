caee47b8d7e3ff0b1eea75e9955954ce7b6095eb
NguyenThanhVinh2711 <vinh99657@gmail.com>
2022-10-14 15:09:07 +0700

    spring

---
+++ src/main/java/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting() {
+//        model.addAttribute("name", name);
+        return "index";
+    }
+}
